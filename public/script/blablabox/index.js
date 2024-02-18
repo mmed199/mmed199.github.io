e5611af56b1272c30b79397c871878f593d93f72
Moussaoui <mohamed.moussaoui@fortil.group>
2024-02-18 23:46:03 +0100

    ✨ BBB project

--- public/script/blablabox/index.js
+++ public/script/blablabox/index.js
@@ -1,58 +1,13 @@
-window.onload = async function() {
-  // Check if the Web NFC API is supported
+window.onload = function() {
   if ('NDEFReader' in window) {
-    // Request NFC permission
-    navigator.permissions.query({ name: 'nfc' }).then(nfcPermission => {
-      const cardScannerTitle = document.querySelector('.card-scanner-title');
-
-      if (nfcPermission.state === 'granted') {
-        // NFC permission granted, you can now use the Web NFC API
-        cardScannerTitle.textContent = 'NFC is supported and permission is granted';
-      } else if (nfcPermission.state === 'prompt') {
-        // NFC permission prompt shown, wait for user's response
-        nfcPermission.onchange = () => {
-          if (nfcPermission.state === 'granted') {
-            cardScannerTitle.textContent = 'NFC is supported and permission is granted';
+      navigator.permissions.query({ name: 'nfc'}).then(result => {
+          if (result.state === 'granted') {
+              document.getElementById('card-scanner-title').innerHTML = 'Scan your card'
           } else {
-            // NFC permission denied, handle accordingly
-            cardScannerTitle.textContent = 'NFC is supported but permission is denied';
+            document.getElementById('card-scanner-title').innerHTML = 'Please allow access to NFC'
           }
-        };
-      } else {
-        // NFC permission denied, handle accordingly
-        cardScannerTitle.textContent = 'NFC is supported but permission is denied';
-      }
-    });
+      })
   } else {
-    // Web NFC API not supported, handle accordingly
-    const cardScannerTitle = document.querySelector('.card-scanner-title');
-    cardScannerTitle.textContent = 'NFC is not supported';
-    console.log('NFC is not supported');
+    document.getElementById('card-scanner-title').innerHTML = 'NFC not supported'
   }
-
-
-  const askPermissionButton = document.querySelector('.ask-permission-button');
-  askPermissionButton.addEventListener('click', async () => {
-    navigator.permissions.query({ name: 'nfc'}).then(async result => {
-      const cardScannerTitle = document.querySelector('.card-scanner-title');
-
-      try {
-        const ndef = new NDEFReader();
-        await ndef.scan();
-        log("> Scan started");
-    
-        ndef.addEventListener("readingerror", () => {
-          log("Argh! Cannot read data from the NFC tag. Try another one?");
-        });
-    
-        ndef.addEventListener("reading", ({ message, serialNumber }) => {
-          log(`> Serial Number: ${serialNumber}`);
-          log(`> Records: (${message.records.length})`);
-        });
-      } catch (error) {
-        log("Argh! " + error);
-      }
-  })
-  });
 }
-
