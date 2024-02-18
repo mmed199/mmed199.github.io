169a6daa2bc6c0a7ac2713d09a4e86dfe440f45a
Moussaoui <mohamed.moussaoui@fortil.group>
2024-02-18 23:56:35 +0100

    ✨ BBB project

--- public/script/blablabox/index.js
+++ public/script/blablabox/index.js
@@ -1,27 +1,28 @@
 window.onload = function() {
-  if (!'NDEFReader' in window) {
+  if (!('NDEFReader' in window)) {
     document.getElementById('card-scanner-title').innerHTML = 'NFC not supported'
-  } 
+  }
+
+  const scanButton = document.getElementById('ask-permission')
+
+  scanButton.addEventListener("click", async () => {
+    document.getElementById('card-scanner-title').innerHTML = "User clicked scan button"
+
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      document.getElementById('card-scanner-title').innerHTML = 'Scan your card'
+
+      ndef.addEventListener("readingerror", () => {
+        document.getElementById('card-scanner-title').innerHTML = "Argh! Cannot read data from the NFC tag. Try another one?"
+      });
+
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        document.getElementById('card-scanner-title').innerHTML = `> Serial Number: ${serialNumber}`
+      });
+    } catch (error) {
+      document.getElementById('card-scanner-title').innerHTML = "Argh! " + error
+    }
+  });
 }
 
-const scanButton = document.getElementById('ask-permission')
-
-scanButton.addEventListener("click", async () => {
-  log("User clicked scan button");
-
-  try {
-    const ndef = new NDEFReader();
-    await ndef.scan();
-    document.getElementById('card-scanner-title').innerHTML = 'Scan your card'
-
-    ndef.addEventListener("readingerror", () => {
-      document.getElementById('card-scanner-title').innerHTML = "Argh! Cannot read data from the NFC tag. Try another one?"
-    });
-
-    ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      document.getElementById('card-scanner-title').innerHTML = `> Serial Number: ${serialNumber}`
-    });
-  } catch (error) {
-    document.getElementById('card-scanner-title').innerHTML = "Argh! " + error
-  }
-});
