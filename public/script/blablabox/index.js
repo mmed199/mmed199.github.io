b85fbe18cbe84466170bac6f2d2572fad7c3c4a9
Moussaoui <mohamed.moussaoui@fortil.group>
2024-02-19 00:06:52 +0100

    ✨ BBB project

--- public/script/blablabox/index.js
+++ public/script/blablabox/index.js
@@ -1,31 +1,43 @@
-const NFC_NOT_SUPPORTED = 'NFC not supported'
+const NFC_NOT_SUPPORTED = {
+  title : 'NFC not supported',
+  button : '/'
+}
+const SCAN_CARD = {
+  title : 'Please scan the attendee card',
+  button : 'Scan'
+}
 
-const SCAN_CARD = 'Please scan the attendee card'
-const ASK_PERMISSION = 'Please allow access to NFC'
+const ASK_PERMISSION = {
+  title : 'Please allow access to NFC to scan the attendee card',
+  button : 'Give permission'
+}
 
 const title = document.getElementById('card-scanner-title');
-
+const button = document.getElementById('card-scanner-button');
 
 window.onload = function() {
   if (!('NDEFReader' in window)) {
-    title.innerHTML = NFC_NOT_SUPPORTED
+    title.innerHTML = NFC_NOT_SUPPORTED.title
+    button.innerHTML = NFC_NOT_SUPPORTED.button
   } else {
     navigator.permissions.query({ name: 'nfc'}).then(result => {
       if (result.state === 'granted') {
-        title.innerHTML = SCAN_CARD
+        title.innerHTML = SCAN_CARD.title
+        button.innerHTML = SCAN_CARD.button
       } else {
-        title.innerHTML = ASK_PERMISSION
+        title.innerHTML = ASK_PERMISSION.title
+        button.innerHTML = ASK_PERMISSION.button
       }
     })
   }
 
-  const scanButton = document.getElementById('ask-permission')
 
-  scanButton.addEventListener("click", async () => {
+  button.addEventListener("click", async () => {
     try {
       const ndef = new NDEFReader();
       await ndef.scan();
-      title.innerHTML = 'Scan your card'
+      title.innerHTML = SCAN_CARD.title
+      button.innerHTML = SCAN_CARD.button
 
       ndef.addEventListener("readingerror", () => {
         title.innerHTML = "Argh! Cannot read data from the atendee card. Try another one?"
