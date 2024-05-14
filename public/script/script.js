3f7929a52c5821dc9b695c5d54d2f4c2ac06f1a2
Mohamed Moussaoui <moussaoui.mohamed.m@gmail.com>
2024-05-14 21:51:18 +0200

    ✨ Get learning section from notion

--- public/script/script.js
+++ public/script/script.js
@@ -3,7 +3,6 @@ window.onload = function() {
     const noisyElements = document.querySelectorAll('.noisy');
     noisyElements.forEach((element) => {
         const borderRadius = window.getComputedStyle(element).getPropertyValue('border-radius') || '0px';
-        console.log(borderRadius);
         element.innerHTML = `<svg width="100%" height="100%" class="noise" style="border-radius: ${borderRadius}"><filter id="noise"><feTurbulence type="fractalNoise" baseFrequency="0.80" numOctaves="4" stitchTiles="stitch"></feTurbulence></filter><rect width="100%" height="100%" filter="url(#noise)"></rect></svg> ${element.innerHTML}`;
         element.style.position = 'relative';
     });
@@ -20,5 +19,39 @@ window.onload = function() {
         localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
     });
 
+
+    // Get Learning
+    const itLearningList = document.querySelector('#it_learning_list');
+    const booksReadingList = document.querySelector('#books_reading_list');
+
+    fetch('https://care-space-42021ec4f14c.herokuapp.com/notion/actual_learning')
+        .then(response => response.json())
+        .then(response => {
+            if (response.it && response.it.length > 0) {
+                itLearningList.innerHTML = response.it.map((item) => `<div>${item}</div>`).join('');
+            } else {
+                itLearningVide();
+            }
+
+            if (response.books && response.books.length > 0) {
+                booksReadingList.innerHTML = response.books.map((item) => `<div>${item}</div>`).join('');
+            } else {
+                booksReadingVide();
+            }
+
+        })
+        .catch(() => {
+            itLearningVide();
+            booksReadingVide();
+        })
 }
 
+function itLearningVide () {
+    const itLearningList = document.querySelector('#it_learning_list');
+    itLearningList.innerHTML = '<div>🤞 Notion API</div><div>☸️ Kubernetes</div>';
+}
+
+function booksReadingVide() {
+    const booksReadingList = document.querySelector('#books_reading_list');
+    booksReadingList.innerHTML = '<div>🕶️ Blindness (novel)</div>';
+}
